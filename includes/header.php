7dccbe4034b13b65072dfd90f438e3c4eda713ff
Imad Asserne <imad.asserne@gmail.com>
2025-05-25 23:33:39 +0000

    fix links

--- includes/header.php
+++ includes/header.php
@@ -48,18 +48,18 @@
         <header class="site-navbar mt-3">
             <div class="container-fluid">
                 <div class="row align-items-center">
-                    <div class="site-logo col-6"><a href="index.html">TechTalent Hub</a></div>
+                    <div class="site-logo col-6"><a href=<?php echo APP_URL ?>>TechTalent Hub</a></div>
 
                     <nav class="mx-auto site-navigation">
                         <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-                            <li><a href="index.html" class="nav-link active">Home</a></li>
-                            <li><a href="about.html">About</a></li>
+                            <li><a href="<?php echo APP_URL ?>" class="nav-link active">Home</a></li>
+                            <li><a href="<?php echo APP_URL ?>/about.php">About</a></li>
 
-                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="<?php echo APP_URL ?>/contact.php">Contact</a></li>
                             <li class="d-lg-none">
-                                <a href="post-job.html"><span class="mr-2">+</span> Post a Job</a>
+                                <a href="<?php echo APP_URL ?>/post-job.php"><span class="mr-2">+</span> Post a Job</a>
                             </li>
-                            <li class="d-lg-none"><a href="#">Log In</a></li>
+                            <li class="d-lg-none"><a href="<?php echo APP_URL ?>/auth/login.php">Log In</a></li>
                         </ul>
                     </nav>
 
@@ -67,7 +67,7 @@
                         class="right-cta-menu text-right d-flex aligin-items-center col-6">
                         <div class="ml-auto">
                             <a
-                                href="post-job.html"
+                                href="<?php echo APP_URL ?>/post-job.php"
                                 class="btn btn-outline-white border-width-2 d-none d-lg-inline-block"><span class="mr-2 icon-add"></span>Post a Job</a>
                             <a
                                 href="<?php echo APP_URL ?>/auth/login.php"
